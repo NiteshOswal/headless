09c69110e8cbd3d64b6296bb4a313bc4e3da94f3
Nitesh Oswal <nit.oswal@gmail.com>
2018-06-09 06:22:44 +0530

    Support for daemons created by root

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require('path');
 require('dotenv').config({
     path: path.join(__dirname, '.env'),
 });
-const server = require('prerender')();
+const server = require('prerender')({
+    chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars']
+});
 server.use(require('prerender-memory-cache'));
 server.start();
